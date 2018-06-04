d9c8500f54ac2086cee0dc9ab39b244bb159a177
Domenico Sironi <domy_siro@hotmail.com>
2018-06-04 17:04:16 +0200

    Progetto completo

---
+++ EXERCISE.CONSOLE/EXERCISE/Exercise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCISE
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        private Player()
+        {
+
+        }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+
+    public class FiveASide
+    {
+        public static int ChooseSides(Player[] players)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
